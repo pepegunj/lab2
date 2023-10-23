7c7b628f8920e6623bfdd3f34fbf18fb8c88ee60
pepegunj <pepegunj@gmail.com>
2023-10-23 08:56:48 +0300

    add copydataset with rename , add create copy with unique name

--- lab2.py
+++ lab2.py
@@ -1,4 +1,5 @@
 import os
+import random
 import requests
 from imageparser import YandexImage
 import csv
@@ -38,7 +39,7 @@ def parseImages(query):
             print(f"Error downloading image from URL {item.url}. Error: {e}")
 
 def create_annotation_file(class_folder, output_file):
-    dataset_folder = 'dataset'  # Путь к исходному датасету
+    dataset_folder = 'dataset'
     with open(output_file, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(['Absolute Path', 'Relative Path', 'Class Label'])
@@ -50,6 +51,35 @@ def create_annotation_file(class_folder, output_file):
                     rel_path = os.path.relpath(abs_path, dataset_folder)
                     label = class_folder
                     writer.writerow([abs_path, rel_path, label])
+
+def copy_dataset_with_rename(class_folder, output_folder):
+    input_folder = 'dataset'
+    for root, dirs, files in os.walk(os.path.join(input_folder, class_folder)):
+        for file in files:
+            if file.endswith('.jpg'):
+                new_name = f"{class_folder}_{file}"
+                old_path = os.path.join(root, file)
+                new_path = os.path.join(output_folder, new_name)
+                shutil.copy(old_path, new_path)
+
+def create_copy_with_unique_random_names():
+    input_folder = 'dataset'
+    output_folder = 'newdataset'  
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+    
+    used_names = set()
+    
+    for root, dirs, files in os.walk(input_folder):
+        for file in files:
+            if file.endswith('.jpg'):
+                random_name = None
+                while random_name is None or random_name in used_names:
+                    random_name = str(random.randint(0, 10000)) + '.jpg'
+                used_names.add(random_name)
+                old_path = os.path.join(root, file)
+                new_path = os.path.join(output_folder, random_name)
+                shutil.copy(old_path, new_path)
 
 
 
